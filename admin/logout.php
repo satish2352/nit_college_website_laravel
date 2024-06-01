5bc71ad415793b93c3b43e0e6756b2194597dadc
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-01 17:05:57 +0530

    change

--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-//header('location:index.php');
-echo "<script>";
-echo "alert('Logout successfull');";
-echo 'window.location.href="index.php";';
-echo "</script>";
-?>
