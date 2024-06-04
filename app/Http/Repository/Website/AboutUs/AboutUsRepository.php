7d43f44bb521f5a39aed9984f62fd9feb16f0ed9
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-04 15:49:44 +0530

    changes

--- app/Http/Repository/Website/AboutUs/AboutUsRepository.php
+++ app/Http/Repository/Website/AboutUs/AboutUsRepository.php
@@ -17,17 +17,18 @@ class AboutUsRepository  {
         try {
 
             $data_output = BoardMembers::join('department', 'department.id', '=', 'tbl_board_members.Designation_id')
-            ->select(
-                'tbl_board_members.fld_bm_id',
-                'department.id as Department_id',
-                'tbl_board_members.fld_bm_name', 
-                'tbl_board_members.gov_address', 
-                'department.Department',
-                'tbl_board_members.is_active'
-            )
-            // ->where('department.id', $id) // Filter by the provided ID
-            ->orderBy('tbl_board_members.fld_bm_id', 'desc')
-            ->first(); // Get a single record
+    ->select(
+        'tbl_board_members.fld_bm_id',
+        'department.id as Department_id',
+        'tbl_board_members.fld_bm_name', 
+        'tbl_board_members.gov_address', 
+        'department.Department',
+        'tbl_board_members.is_active'
+    )
+    // ->where('department.id', $id) // Uncomment this line to filter by the provided department ID
+    ->orderBy('tbl_board_members.fld_bm_id', 'desc')
+    ->get(); // Use get() to retrieve all matching records
+
 
            
                       return $data_output;
