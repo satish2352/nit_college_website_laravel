146fb91d61274ea4876e1a6146cdcafb0c56f2db
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-11 17:29:10 +0530

    changes

--- resources/views/website/pages/aboutus/polytechnic-about-governingbody.blade.php
+++ resources/views/website/pages/aboutus/polytechnic-about-governingbody.blade.php
@@ -167,7 +167,7 @@
                                         <center>
 
                                             <table id="order-listing" class="table table-striped table-hover table-bordered border-dark">
-                                                <thead class="" style="background-color: #47194a; color:#fff">
+                                                <thead class="" >
                                                     <tr>
                                                         <th scope="col" class="d-flex justify-content-center">
                                                             SR.NO
