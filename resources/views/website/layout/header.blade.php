b4af70d0408bb5369c24b47329ffd42c16852f99
gunjansalunke <salunkegunjan@gmail.com>
2024-06-13 15:03:23 +0530

    changes in department

--- resources/views/website/layout/header.blade.php
+++ resources/views/website/layout/header.blade.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('public/assets/visitors/hm/design/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <style>
       .heading1 {
     font-size: 20px;
