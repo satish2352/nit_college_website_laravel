0a9571ae5eb7df25ae186bcd3305bb40b3577e60
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-19 16:13:26 +0530

    changes

--- resources/views/website/pages/home-sidebar/polytechnic-calendar.blade.php
+++ resources/views/website/pages/home-sidebar/polytechnic-calendar.blade.php
@@ -16,22 +16,62 @@
                                 <div class="panel-heading">
                                     <center>
                                         <h3 class="panel-title" style="color:blue"><b>Academic Calendar</b></h3>
-                                        <embed src="{{ asset('http://nitnasik.com/nitadmin/assets/documents/academiccalendar/667274fd3a639A.Y.%202024-25_AcademicCalendar_16052024_1104.pdf') }}" type="application/pdf" width="100%" height="600px" />
                                     </center>
                                     <span class="pull-right clickable">
                                         <i class="glyphicon glyphicon-plus-sign collapse-ico"></i>
                                     </span>
                                 </div>
-                                <div class="panel-body">
+                                <center>
+                                    <div class="table-responsive p-3 pt-5">
+                                        <!-- Add this wrapper -->
+                                        <table id="example"
+                                            class="table display responsive nowrap table-striped table-hover table-bordered border-dark"
+                                            style="width:100%">
+                                            <thead class="">
+                                                <tr>
+                                                    <th scope="col" class=" justify-content-center">
+                                                        <center>SR.NO</center>
+                                                    </th>
+                                                    <th scope="col">
+                                                        <div class="d-flex justify-content-center">
+                                                            Download File
+                                                        </div>
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @forelse ($data_output as $data)
+                                                <tr class="">
+                                                    <td>
+                                                        <center>{{ $loop->iteration }}</center>
+                                                    </td>
+                                                    <td>
+                                                        <center>
+                                                            <a href="{{ Config::get('DocumentConstant.ACADEMICCALENDAR_VIEW') }}{{ $data->file }}"
+                                                                target="_blank" class="btn btn-small btn-primary">
+                                                                <i class="btn-icon-only icon-ok">Download</i>
+                                                            </a>
+                                                        </center>
+                                                    </td>
+                                                </tr>
+                                                @empty
+                                                {{-- <h4>No Data Found</h4> --}}
+                                                @endforelse
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </center>
+                                {{-- <div class="panel-body">
                                     <center>
                                         @forelse($data_output as $data)
+
                                             <iframe src="{{ url(Config::get('DocumentConstant.ACADEMICCALENDAR_VIEW') . $data->file) }}" width="100%" height="800px"></iframe>
                                             <p>{{ url(Config::get('DocumentConstant.ACADEMICCALENDAR_VIEW') . $data->file) }}</p> <!-- Add this line for debugging -->
                                         @empty
                                             <p>No data found</p>
                                         @endforelse
                                     </center>
-                                </div>
+                                </div> --}}
                                 
                                 
                             </div>
