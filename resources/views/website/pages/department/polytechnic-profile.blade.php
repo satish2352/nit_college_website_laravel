2014d2cb6daab0b3d3661be7ce35b420b3bec539
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-19 15:23:14 +0530

    changes

--- resources/views/website/pages/department/polytechnic-profile.blade.php
+++ resources/views/website/pages/department/polytechnic-profile.blade.php
@@ -6,95 +6,64 @@
                 <h4 style="color:green"><strong>Welcome To Humanities & Science Engineering Department </strong></h4>
             </div>
         </div> --}}
-    </div>
-    <div class="container-fluid" style="padding-bottom:20px">
-        <div class="container card-shadow">
-          
-                <div class="row" style="padding:0px ">
-                    <div class="col-md-9 col-sm-12 text-justify" id="iwtContentArea">
-                        <div class="row outer-white">
-                        </div>
-                        <div class="row outer-white">
-                            <div class="col-lg-12 col-sm-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div>
-                                            @if (isset($error))
-                                            <div>
-                                                {{ $error }}
-                                            </div>
-                                        @else
-                                            @if ($data_output->isEmpty())
-                                                <div>
-                                                     <p class="department-error">Data not available.</p>
-                                                </div>
-                                            @else
-                                                @foreach ($data_output as $data_output)
-                                                    @if ($data_output->is_active == 0)
-                                                        <div class="alert alert-info">
-                                                             <p class="department-error">Data not available.</p>
-                                                        </div>
-                                                    @else
-                                                        <div>
-                                                            <div class="" style="color:#00ae97; font-size:20px;">{{$data_output->Department}}</div>
-
-        <div class="row" style="padding:0px ">
+</div>
+<div class="container-fluid" style="padding-bottom:20px">
+    <div class="container card-shadow">
+        <div class="row" style="padding:0px">
             <div class="col-md-9 col-sm-12 text-justify" id="iwtContentArea">
-                <div class="row outer-white">
-                </div>
+                <div class="row outer-white"></div>
                 <div class="row outer-white">
                     <div class="col-lg-12 col-sm-12">
                         <div class="card">
                             <div class="card-body">
                                 <div>
                                     @if (isset($error))
-                                    <div>
-                                        {{ $error }}
-                                    </div>
+                                        <div>
+                                            {{ $error }}
+                                        </div>
                                     @else
-                                    @if ($data_output->isEmpty())
-                                    <div>
-                                        <p class="department-error">Data not available.</p>
-                                    </div>
-                                    @else
-                                    @foreach ($data_output as $data_output)
-                                    @if ($data_output->is_active == 0)
-                                    <div class="alert alert-info">
-                                        <p class="department-error">Data not available.</p>
-                                    </div>
-                                    @else
-                                    <div>
-                                        <div class="" style="color:#00ae97;">{{$data_output->Department}}</div>
-
-                                        {{-- <h4 style="color:black">{{ $data_output->Department }}</h4> --}}
-                                    </div>
-                                    <div>
-                                        <p>{{ strip_tags($data_output->department_description) }}</p>
-                                    </div>
-                                    <img id="english"
-                                        src="{{ Config::get('DocumentConstant.DEPARTMENT_VIEW_PROFILE') }}{{ $data_output->photo }}"
-                                        class="img-fluid img-thumbnail" height="300px" width="700px">
-                                    @endif
-                                    @endforeach
-                                    @endif
+                                        @if ($data_output->isEmpty())
+                                            <div>
+                                                <p class="department-error">Data not available.</p>
+                                            </div>
+                                        @else
+                                            @foreach ($data_output as $data)
+                                                @if ($data->is_active == 0)
+                                                    <div class="alert alert-info">
+                                                        <p class="department-error">Data not available.</p>
+                                                    </div>
+                                                @else
+                                                    <div>
+                                                        <div class="" style="color:#00ae97; font-size:20px;">
+                                                            {{ $data->Department }}
+                                                        </div>
+                                                        <div>
+                                                            <p>{{ strip_tags($data->department_description) }}</p>
+                                                        </div>
+                                                        <img id="english"
+                                                             src="{{ Config::get('DocumentConstant.DEPARTMENT_VIEW_PROFILE') }}{{ $data->photo }}"
+                                                             class="img-fluid img-thumbnail"
+                                                             height="300px" width="700px">
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        @endif
                                     @endif
                                     <div>
                                         <h2></h2>
                                     </div>
-
                                     <p></p>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
             <div class="col-md-3 hidden-sm right-col-fix" id="iwtRightPannel">
                 @include('website.pages.department.right-sidebar-department')
             </div>
         </div>
-
     </div>
-</div><br><br><br><br>
+</div>
+<br><br><br><br>
 @endsection
