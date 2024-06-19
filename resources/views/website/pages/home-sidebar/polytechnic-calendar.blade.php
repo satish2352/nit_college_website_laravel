a869b4385d1bdb9ed83a7268c5cd4d8c0b32a6fc
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-19 16:14:55 +0530

    changes

--- resources/views/website/pages/home-sidebar/polytechnic-calendar.blade.php
+++ resources/views/website/pages/home-sidebar/polytechnic-calendar.blade.php
@@ -11,7 +11,7 @@
                     </div>
 
                     <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-                        <div class="col-lg-9 col-md-9 col-sm-9">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
                             <div class="panel panel-primary product-type">
                                 <div class="panel-heading">
                                     <center>
